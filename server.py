bcf0542b338b1f5e3fa2515cc70ca31ad43d3eec
Subham Sidhanta <94817723+Subhamsidhanta@users.noreply.github.com>
2026-05-28 18:39:03 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, WebSocket
 import uvicorn
+import os
 
 app = FastAPI()
 
@@ -21,14 +22,21 @@ async def websocket_endpoint(websocket: WebSocket):
             print("Received:", data)
 
             for client in clients:
-                await client.send_text(data)
+
+                # Don't send back to sender
+                if client != websocket:
+                    await client.send_text(data)
 
     except Exception as e:
         print("Error:", e)
 
     finally:
+
         clients.remove(websocket)
         print("Client disconnected")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
